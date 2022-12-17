41bb5687bc6a31e9ac94b80755556fbce5317db1
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-12-17 22:38:58 +0100

    v0.2.7

--- plugin.js
+++ plugin.js
@@ -1,22 +1,3 @@
-/**
- * ThePirateBay plugin for Showtime
- *
- *  Copyright (C) 2014-2016 Wain, 2022 fix by BitZy
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 (function (plugin) {
     var config = {
         pluginInfo: plugin.getDescriptor(),
@@ -50,7 +31,6 @@
     }
 
     plugin.addURI(config.prefix + ":start", function (page) {
-        //Pages: browse, recent, tv shows, Music, Top 100
         var pages = [
                 {
                     url: '/browse',
@@ -58,11 +38,24 @@
                 },
                 {
                     url: '/recent',
-                    name: 'Recent'
+                    name: 'Recent uploads'
+                },
+                {
+                    url: '/top/200',
+                    name: 'Top 100 (Movies)'
+                },
+                {
+                    url: '/top/208',
+                    name: 'Top 100 (TV Shows)'
+                },
+                {
+                    url: '/top/101',
+                    name: 'Top 100 (Music)'
                 },
                 {
                     url: '/browse/205',
                     name: 'TV Shows'
+
                 },
                 {
                     url: '/browse/201',
@@ -72,10 +65,7 @@
                     url: '/browse/101',
                     name: 'Music'
                 },
-                {
-                    url: '/top/all',
-                    name: 'Top 100'
-                }
+
             ],
             i, length = pages.length;
         setPageHeader(page, config.pluginInfo.synopsis);
@@ -167,8 +157,8 @@
     });
 
     plugin.addSearcher(plugin.getDescriptor().id, config.logo, function (page, query) {
-        //Example URL: https://thepiratebay.se/search/one/0/99/0
-        var url = config.urls.base + '/search/' + encodeURIComponent(query) + '/0/99/0',
+        //Example URL: https://www1.thepiratebay3.to/s/?q=Avatar&category=0
+        var url = config.urls.base + '/s/?q' + encodeURIComponent(query) + '&category=0',
             nextUrl, tryToSearch = true, i,
             doc, dom, link, linkUrl, linkTitle, items;
 
